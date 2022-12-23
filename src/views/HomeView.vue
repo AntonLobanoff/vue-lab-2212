235539786a470ab03c96021db9919eb54de3edb0
Danil <danil@azor.kg>
2022-12-23 16:42:09 +0600

    Fix homework

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,37 @@
 <template>
   <layout>
     <promo :promo="promo"/>
+    <div class="container">
+      <div class="card">
+        <div class="image-wrap" @dblclick="liked = !liked">
+          <img src="https://purepng.com/public/uploads/large/purepng.com-spider-manspider-manspidermansuperherocomic-bookmarvel-comicscharacterstan-lee-1701528655227svxq2.png" alt="">
+        </div>
+        <button class="heart" @click="liked = !liked">
+          <svg :class="{liked: liked}" x="0px" y="0px" viewBox="0 0 471.701 471.701" style="enable-background:new 0 0 471.701 471.701;">
+            <g>
+              <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1
+                  c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3
+                  l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4
+                  C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3
+                  s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4
+                  c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3
+                  C444.801,187.101,434.001,213.101,414.401,232.701z"/>
+            </g>
+          </svg>
+        </button>
+      </div>
+      <div>
+        <input v-model="name" type="text" placeholder="name">
+        <textarea v-model="comment" placeholder="comment"></textarea>
+        <button @click="send">send</button>
+      </div>
+      <div v-if="commentsList.length">
+        <div v-for="list in commentsList">
+          <p>{{ list.name }}</p>
+          <p>{{ list.comment }}</p>
+        </div>
+      </div>
+    </div>
   </layout>
 </template>
 <script>
@@ -10,8 +41,55 @@ export default {
       promo: {
         title: 'Home',
         image: 'https://static-cse.canva.com/blob/572026/removingbackgroundimages_Unsplash.jpeg'
+      },
+      liked: false,
+      name: '',
+      comment: '',
+      commentsList: []
+    }
+  },
+  methods: {
+    send() {
+      if(this.name.length && this.comment.length) {
+        this.commentsList.push({
+          name: this.name,
+          comment: this.comment
+        })
       }
+      this.name = ''
+      this.comment = ''
     }
   }
 }
 </script>
+<style lang="scss">
+.card {
+  padding: 40px 0;
+  width: 250px;
+  .image-wrap {
+    width: 100%;
+    height: 250px;
+    border: 1px solid #545454;
+    margin-bottom: 15px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+  .heart {
+    width: 40px;
+    height: 40px;
+    padding: 0;
+    background: transparent;
+    border: none;
+    svg {
+      width: 100%;
+      height: 100%;
+      &.liked {
+        fill: red;
+      }
+    }
+  }
+}
+</style>
